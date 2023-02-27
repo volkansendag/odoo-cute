b56145ffd95a70093e2f0c2e38e57c781725b8d5
Volkan Şendağ <vsendag@gmail.com>
2023-02-27 21:09:55 +0300

    izin formu izin sebebi

--- addons-cute/hr_leave_improve/__manifest__.py
+++ addons-cute/hr_leave_improve/__manifest__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'Time Off Improve ',
+    'name': 'İzin Modülü Ek Özellikler',
     'version': '1.5',
-    'category': 'Human Resources/Time Off Improve',
+    'category': 'Human Resources/İzin Modülü Ek Özellikler',
     'sequence': 85,
     'summary': 'Izin modulu icin yeni gelistirmeler',
     'website': 'https://volkansendag.com',
@@ -18,9 +18,10 @@ Yapilacak calismalar
     'depends': ['hr', 'resource'],
     'data': [
         'security/hr_leave_improve_security.xml',
-        'views/hr_leave_improve_view.xml',
         'views/hr_leave_improve_reports.xml',
-        'views/hr_leave_improve_mail.xml'
+        'views/hr_leave_improve_mail.xml',
+        'views/hr_employee_improve_view.xml',
+        'views/hr_leave_type_improve_view.xml'
     ],
     'demo': [],
     'installable': True,
