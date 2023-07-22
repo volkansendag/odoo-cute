553feda6e744e3552c47bc41d895729a059c3002
Volkan Şendağ <vsendag@gmail.com>
2023-07-22 07:26:58 +0300

    3part addons added.

---
+++ addons-3part/hide_powered_by_odoo/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Powered By Odoo Gizle',
+    'version': '15.0.1.0.0',
+    'sequence': 1,
+    'summary': """
+        Giriş ekranında Powered By Odoo linkini gizle.
+    """,
+    'description': "Giriş ekranında Powered By Odoo linkini gizle.",
+    'author': 'Belsis Solutions',
+    'maintainer': 'Belsis Solutions',
+    'price': '0.0',
+    'currency': 'TRY',
+    'website': 'https://belsis.com.tr',
+    'license': 'LGPL-3',
+    'depends': [
+        'web'
+    ],
+    'data': [
+        'views/login_templates.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
